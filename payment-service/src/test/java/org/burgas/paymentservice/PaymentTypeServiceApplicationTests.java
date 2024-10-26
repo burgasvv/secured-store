41eb07edeba13bf664ac6ee7925a8b9cd347261b
Burgas <burgassme@gmail.com>
2024-10-26 15:34:39 +0700

    Initial commit

---
+++ payment-service/src/test/java/org/burgas/paymentservice/PaymentTypeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.burgas.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTypeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
